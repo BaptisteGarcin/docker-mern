905a9bc711562436d04e038b907bad1b171c62ce
Philipp Alferov <philipp.alferov@gmail.com>
2016-10-11 22:49:04 -0400

    Add dummy mongoose model

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
 mongoose.connect('mongodb://db:27017/express-mongo');
 
